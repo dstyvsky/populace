a79d4fc77f73c02b0f19d1cc1497eb5270dbf733
fyodor <dstyvsky@gmail.com>
2016-03-25 18:02:45 -0400

    Updated who can edit and delete posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :destroy]	
 
   # GET /posts
   # GET /posts.json
@@ -86,5 +87,9 @@ class PostsController < ApplicationController
     def quick_image
      
     end
-
+	
+		def correct_user
+			@post = current_user.posts.find_by(id: params[:id])
+			redirect_to root_url if @post.nil?
+		end
 end
